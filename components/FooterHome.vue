3eb68d7525677e3b8b4972091c3c4d12eab09f30
Alan Raza <alanraza66@gmail.com>
2023-12-26 19:46:53 +0300

    fix: :lipstick: Height mobile.

--- components/FooterHome.vue
+++ components/FooterHome.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid w-full h-full grid-cols-3 grid-rows-4 gap-4 text-light md:grid-rows-6 md:gap-3"
+    class="grid w-full h-full grid-cols-3 grid-rows-4 gap-4 text-light md:grid-rows-4 md:gap-3"
   >
     <div
       class="relative col-span-2 row-span-4 overflow-hidden rounded cursor-pointer overlay-activator bg-third md:col-span-5 md:row-span-2 md:aspect-video"
--- pages/index.vue
+++ pages/index.vue
@@ -8,22 +8,22 @@ gsap.registerPlugin(ScrollTrigger);
 <template>
   <main class="w-full min-h-screen">
     <div
-      class="h-screen w-full bg-primary pt-[96px] pb-[1rem] px-[1rem] md:min-h-screen md:h-auto md:pt-[56px] md:pb-[0.75rem] md:px-[0.75rem] relative"
+      class="h-screen w-full bg-primary pt-[96px] pb-[1rem] px-[1rem] md:h-auto md:pt-[56px] md:pb-[0.75rem] md:px-[0.75rem] relative"
     >
       <HeroHome />
     </div>
     <div
-      class="h-screen w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:min-h-screen md:h-auto relative"
+      class="h-screen w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:h-auto relative"
     >
       <StudyHome />
     </div>
     <div
-      class="h-screen w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:min-h-screen md:h-auto relative"
+      class="h-screen w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:h-auto relative"
     >
       <DevStackHome />
     </div>
     <div
-      class="h-[50vh] w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:min-h-screen md:h-auto relative"
+      class="h-[50vh] w-full bg-primary pb-[1rem] px-[1rem] md:px-[0.75rem] md:pb-[0.75rem] md:h-auto relative"
     >
       <FooterHome />
     </div>
