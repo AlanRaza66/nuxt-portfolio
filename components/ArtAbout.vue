78d6523a399cf8bc5c65e7277cf133b9d62380fe
Alan Raza <alanraza66@gmail.com>
2023-12-29 17:42:31 +0300

    feat: :sparkles: Last part with animation.

--- components/ArtAbout.vue
+++ components/ArtAbout.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import gsap from "gsap";
+
+const main = ref();
+let ctx: gsap.Context;
+onMounted(() => {
+  ctx = gsap.context((self) => {
+    const components = self.selector ? self.selector(".component") : null;
+    components.forEach((component: any, y: any) => {
+      gsap.fromTo(
+        component,
+        {
+          x: y % 2 == 0 ? -100 : 100,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: component,
+            start: "top bottom",
+            end: "top 20%",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, main.value); // <- Scope!
+});
+
+onUnmounted(() => {
+  ctx.revert(); // <- Easy Cleanup!
+});
+</script>
 <template>
   <div
     class="grid w-full h-full grid-cols-5 grid-rows-5 gap-4 overflow-x-hidden text-light md:grid-rows-6 md:gap-3"
@@ -31,24 +66,9 @@
       class="relative col-span-2 row-span-2 p-8 overflow-hidden rounded component bg-third md:col-span-5 md:row-span-2 md:aspect-auto md:p-3 overlay-activator"
     >
       <img
-        src="../assets/images/cnam.jpg"
+        src="../assets/images/books.jpg"
         class="absolute top-0 left-0 object-cover w-full h-full"
-      /><NuxtLink
-        class="absolute top-0 left-0 block w-full h-full duration-500 translate-y-[100%] md:translate-y-[0] overlay"
-        to="https://www.cnam-paris.fr/"
-        target="_blank"
-      >
-        <div
-          class="flex items-start justify-end flex-col w-full h-full p-8 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.75)] md:p-3"
-        >
-          <h6 class="text-lg font-bold">
-            Conservatoire Nationale des Arts et Métiers (CNAM)
-          </h6>
-          <p class="text-base font-light md:hidden">
-            Where I pursued my academic journey.
-          </p>
-        </div></NuxtLink
-      >
+      />
     </div>
     <div
       class="col-span-3 row-span-2 p-8 rounded component bg-third md:col-span-5 md:row-span-2 md:aspect-auto md:p-3"
@@ -56,11 +76,13 @@
       <div
         class="flex flex-col items-start justify-center w-full h-full md:justify-start"
       >
-        <h2 class="mb-2 text-4xl font-bold md:text-2xl">Project Manager</h2>
+        <h2 class="mb-2 text-4xl font-bold md:text-2xl">
+          Eager for knowledge.
+        </h2>
         <p class="text-xl font-thin text-justify md:text-base">
-          I pursued a degree in Information Systems Engineering / Business
-          Intelligence with a focus on Project Management at CNAM Madagascar -
-          La Réunion and Paris
+          I enjoy acquiring new knowledge, whether through books, documentaries,
+          or games like "Question Pour Un Champion." I take pleasure in
+          expanding my understanding and learning from various sources.
         </p>
       </div>
     </div>
